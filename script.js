3a6bfeb793d65bd4228f24dbb097ca6a14ba7ade
Cheekymohnkey <ryan.clarke.nz@outlook.com>
2024-12-11 02:21:05 +1300

    Worked on Quiz and also fixed Basic Attack

--- script.js
+++ script.js
@@ -165,7 +165,11 @@ function renderHeroView(hero) {
       </div>
       <div class="personality class-attribute">
         <h2>Personality</h2>
-        <p>${hero.personality}</p>
+        <p>${markdown(hero.personality)}</p>
+      </div>
+      <div class="personality class-attribute">
+        <h2>Super Critical Absolutely Crucial 100% Accurate Personality Test</h2>
+        <p>${markdown(hero.quiz)}</p>
       </div>
       <div class="favourite-phrases class-attribute">
         <h2>Favourite Phrases</h2>
@@ -231,8 +235,12 @@ function renderEditMode(hero) {
       <textarea id="editAppearance">${hero.appearance.description}</textarea>
     </div>
     <div class="edit-section">
-      <label for="editBasicAttack">Basic Attack:</label>
-      <textarea id="editBasicAttack">${hero.basicAttack}</textarea>
+      <label for="editBasicAttackType">Basic Attack Type:</label>
+      <select id="editBasicAttackType">
+         <option value="contact" ${hero.basicAttack.type === "contact ? 'selected' : ''"}>Close contact attack</option>
+         <option value="ranged" ${hero.basicAttack.type === "ranged ? 'selected' : ''"}>Ranged attack</option>
+      </select>
+      <textarea id="editBasicAttackDescription">${hero.basicAttack.description}</textarea>
     </div>
     <div class="edit-section">
       <label for="editAbilityProgression">Ability Progression:</label>
@@ -274,6 +282,14 @@ function renderEditMode(hero) {
        
         </div>
       <button id="addLevelButton">Add Level</button>
+    </div>
+    <div class="edit-section">
+      <label for="editPersonality">Personality:</label>
+      <textarea id="editPersonality">${hero.personality}</textarea>
+    </div>
+    <div class="edit-section">
+      <label for="editQuiz">Super Critical Absolutely Crucial 100% Accurate Personality Test:</label>
+      <textarea id="editQuiz">${hero.quiz}</textarea>
     </div>
     <div class="edit-section">
       <label for="editFavouritePhrases">Favourite Phrases:</label>
@@ -570,9 +586,12 @@ function saveChanges() {
   hero.primaryClass = document.getElementById('editPrimaryClass').value;
   hero.secondaryClass = document.getElementById('editSecondaryClass').value;
   hero.appearance.description = document.getElementById('editAppearance').value;
-  hero.basicAttack = document.getElementById('editBasicAttack').value;
+  hero.basicAttack.type = document.getElementById('editBasicAttackType').value;
+  hero.basicAttack.description = document.getElementById('editBasicAttackDescription').value;
   hero.background = document.getElementById('editBackground').value;
   hero.mantra = document.getElementById('editMantra').value;
+  hero.personality = document.getElementById('editPersonality').value;
+  hero.quiz = document.getElementById('editQuiz').value;
   hero.outro = document.getElementById('editOutro').value;
 
   // Update abilities
@@ -782,10 +801,24 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 });
 // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
 let isAdmin = params.admin;
+let heroId = params.heroid;
 if (isAdmin == "true") {
   var adminDiv = document.getElementById('admincontrols');
   adminDiv.style = "";
 }
+
+if(heroId.length > 0){
+  selectElement("heroSelect", heroId);
+  hero = heroes.find(hero => hero.id == heroId);
+  renderHeroView(hero);
+  renderEditMode(hero);
+}
+
+function selectElement(id, valueToSelect) {    
+  let element = document.getElementById(id);
+  element.value = valueToSelect;
+}
+
 
 function renderHeroList(heroes) {
   const heroesContainer = document.getElementById('heroes-container');
@@ -855,3 +888,133 @@ const checkIsIOS = () =>
 if (checkIsIOS()) {
   disableIosTextFieldZoom();
 }
+
+/**
+ * drawdown.js
+ * (c) Adam Leggett
+ */
+
+
+;function markdown(src) {
+
+  var rx_lt = /</g;
+  var rx_gt = />/g;
+  var rx_space = /\t|\r|\uf8ff/g;
+  var rx_escape = /\\([\\\|`*_{}\[\]()#+\-~])/g;
+  var rx_hr = /^([*\-=_] *){3,}$/gm;
+  var rx_blockquote = /\n *&gt; *([^]*?)(?=(\n|$){2})/g;
+  var rx_list = /\n( *)(?:[*\-+]|((\d+)|([a-z])|[A-Z])[.)]) +([^]*?)(?=(\n|$){2})/g;
+  var rx_listjoin = /<\/(ol|ul)>\n\n<\1>/g;
+  var rx_highlight = /(^|[^A-Za-z\d\\])(([*_])|(~)|(\^)|(--)|(\+\+)|`)(\2?)([^<]*?)\2\8(?!\2)(?=\W|_|$)/g;
+  var rx_code = /\n((```|~~~).*\n?([^]*?)\n?\2|((    .*?\n)+))/g;
+  var rx_link = /((!?)\[(.*?)\]\((.*?)( ".*")?\)|\\([\\`*_{}\[\]()#+\-.!~]))/g;
+  var rx_table = /\n(( *\|.*?\| *\n)+)/g;
+  var rx_thead = /^.*\n( *\|( *\:?-+\:?-+\:? *\|)* *\n|)/;
+  var rx_row = /.*\n/g;
+  var rx_cell = /\||(.*?[^\\])\|/g;
+  var rx_heading = /(?=^|>|\n)([>\s]*?)(#{1,6}) (.*?)( #*)? *(?=\n|$)/g;
+  var rx_para = /(?=^|>|\n)\s*\n+([^<]+?)\n+\s*(?=\n|<|$)/g;
+  var rx_stash = /-\d+\uf8ff/g;
+
+  function replace(rex, fn) {
+      src = src.replace(rex, fn);
+  }
+
+  function element(tag, content) {
+      return '<' + tag + '>' + content + '</' + tag + '>';
+  }
+
+  function blockquote(src) {
+      return src.replace(rx_blockquote, function(all, content) {
+          return element('blockquote', blockquote(highlight(content.replace(/^ *&gt; */gm, ''))));
+      });
+  }
+
+  function list(src) {
+      return src.replace(rx_list, function(all, ind, ol, num, low, content) {
+          var entry = element('li', highlight(content.split(
+              RegExp('\n ?' + ind + '(?:(?:\\d+|[a-zA-Z])[.)]|[*\\-+]) +', 'g')).map(list).join('</li><li>')));
+
+          return '\n' + (ol
+              ? '<ol start="' + (num
+                  ? ol + '">'
+                  : parseInt(ol,36) - 9 + '" style="list-style-type:' + (low ? 'low' : 'upp') + 'er-alpha">') + entry + '</ol>'
+              : element('ul', entry));
+      });
+  }
+
+  function highlight(src) {
+      return src.replace(rx_highlight, function(all, _, p1, emp, sub, sup, small, big, p2, content) {
+          return _ + element(
+                emp ? (p2 ? 'strong' : 'em')
+              : sub ? (p2 ? 's' : 'sub')
+              : sup ? 'sup'
+              : small ? 'small'
+              : big ? 'big'
+              : 'code',
+              highlight(content));
+      });
+  }
+
+  function unesc(str) {
+      return str.replace(rx_escape, '$1');
+  }
+
+  var stash = [];
+  var si = 0;
+
+  src = '\n' + src + '\n';
+
+  replace(rx_lt, '&lt;');
+  replace(rx_gt, '&gt;');
+  replace(rx_space, '  ');
+
+  // blockquote
+  src = blockquote(src);
+
+  // horizontal rule
+  replace(rx_hr, '<hr/>');
+
+  // list
+  src = list(src);
+  replace(rx_listjoin, '');
+
+  // code
+  replace(rx_code, function(all, p1, p2, p3, p4) {
+      stash[--si] = element('pre', element('code', p3||p4.replace(/^    /gm, '')));
+      return si + '\uf8ff';
+  });
+
+  // link or image
+  replace(rx_link, function(all, p1, p2, p3, p4, p5, p6) {
+      stash[--si] = p4
+          ? p2
+              ? '<img src="' + p4 + '" alt="' + p3 + '"/>'
+              : '<a href="' + p4 + '">' + unesc(highlight(p3)) + '</a>'
+          : p6;
+      return si + '\uf8ff';
+  });
+
+  // table
+  replace(rx_table, function(all, table) {
+      var sep = table.match(rx_thead)[1];
+      return '\n' + element('table',
+          table.replace(rx_row, function(row, ri) {
+              return row == sep ? '' : element('tr', row.replace(rx_cell, function(all, cell, ci) {
+                  return ci ? element(sep && !ri ? 'th' : 'td', unesc(highlight(cell || ''))) : ''
+              }))
+          })
+      )
+  });
+
+  // heading
+  replace(rx_heading, function(all, _, p1, p2) { return _ + element('h' + p1.length, unesc(highlight(p2))) });
+
+  // paragraph
+  replace(rx_para, function(all, content) { return element('p', unesc(highlight(content))) });
+
+  // stash
+  replace(rx_stash, function(all) { return stash[parseInt(all)] });
+
+  return src.trim();
+};
