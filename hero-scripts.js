b4f48adfd7b3c2ea0f2570cc4e22d607c0926674
cheekymohnkey <ryan.clarke.nz@outlook.com>
2024-12-11 23:26:32 +1300

    Still on printing

--- hero-scripts.js
+++ hero-scripts.js
@@ -422,13 +422,13 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
 let heroId = params.heroid;
 
-if(heroId.length > 0){
-  selectElement("heroSelect", heroId);
-  hero = heroes.find(hero => hero.id == heroId);
-  renderHeroView(hero);
+if (heroId) { 
+    selectElement("heroSelect", heroId);
+    hero = heroes.find(hero => hero.id == heroId);
+    renderHeroView(hero);
 }
 
-function selectElement(id, valueToSelect) {    
+function selectElement(id, valueToSelect) {
   let element = document.getElementById(id);
   element.value = valueToSelect;
 }
@@ -439,7 +439,7 @@ function selectElement(id, valueToSelect) {
  */
 
 
-;function markdown(src) {
+; function markdown(src) {
 
   var rx_lt = /</g;
   var rx_gt = />/g;
@@ -461,47 +461,47 @@ function selectElement(id, valueToSelect) {
   var rx_stash = /-\d+\uf8ff/g;
 
   function replace(rex, fn) {
-      src = src.replace(rex, fn);
+    src = src.replace(rex, fn);
   }
 
   function element(tag, content) {
-      return '<' + tag + '>' + content + '</' + tag + '>';
+    return '<' + tag + '>' + content + '</' + tag + '>';
   }
 
   function blockquote(src) {
-      return src.replace(rx_blockquote, function(all, content) {
-          return element('blockquote', blockquote(highlight(content.replace(/^ *&gt; */gm, ''))));
-      });
+    return src.replace(rx_blockquote, function (all, content) {
+      return element('blockquote', blockquote(highlight(content.replace(/^ *&gt; */gm, ''))));
+    });
   }
 
   function list(src) {
-      return src.replace(rx_list, function(all, ind, ol, num, low, content) {
-          var entry = element('li', highlight(content.split(
-              RegExp('\n ?' + ind + '(?:(?:\\d+|[a-zA-Z])[.)]|[*\\-+]) +', 'g')).map(list).join('</li><li>')));
+    return src.replace(rx_list, function (all, ind, ol, num, low, content) {
+      var entry = element('li', highlight(content.split(
+        RegExp('\n ?' + ind + '(?:(?:\\d+|[a-zA-Z])[.)]|[*\\-+]) +', 'g')).map(list).join('</li><li>')));
 
-          return '\n' + (ol
-              ? '<ol start="' + (num
-                  ? ol + '">'
-                  : parseInt(ol,36) - 9 + '" style="list-style-type:' + (low ? 'low' : 'upp') + 'er-alpha">') + entry + '</ol>'
-              : element('ul', entry));
-      });
+      return '\n' + (ol
+        ? '<ol start="' + (num
+          ? ol + '">'
+          : parseInt(ol, 36) - 9 + '" style="list-style-type:' + (low ? 'low' : 'upp') + 'er-alpha">') + entry + '</ol>'
+        : element('ul', entry));
+    });
   }
 
   function highlight(src) {
-      return src.replace(rx_highlight, function(all, _, p1, emp, sub, sup, small, big, p2, content) {
-          return _ + element(
-                emp ? (p2 ? 'strong' : 'em')
-              : sub ? (p2 ? 's' : 'sub')
-              : sup ? 'sup'
+    return src.replace(rx_highlight, function (all, _, p1, emp, sub, sup, small, big, p2, content) {
+      return _ + element(
+        emp ? (p2 ? 'strong' : 'em')
+          : sub ? (p2 ? 's' : 'sub')
+            : sup ? 'sup'
               : small ? 'small'
-              : big ? 'big'
-              : 'code',
-              highlight(content));
-      });
+                : big ? 'big'
+                  : 'code',
+        highlight(content));
+    });
   }
 
   function unesc(str) {
-      return str.replace(rx_escape, '$1');
+    return str.replace(rx_escape, '$1');
   }
 
   var stash = [];
@@ -524,41 +524,41 @@ function selectElement(id, valueToSelect) {
   replace(rx_listjoin, '');
 
   // code
-  replace(rx_code, function(all, p1, p2, p3, p4) {
-      stash[--si] = element('pre', element('code', p3||p4.replace(/^    /gm, '')));
-      return si + '\uf8ff';
+  replace(rx_code, function (all, p1, p2, p3, p4) {
+    stash[--si] = element('pre', element('code', p3 || p4.replace(/^    /gm, '')));
+    return si + '\uf8ff';
   });
 
   // link or image
-  replace(rx_link, function(all, p1, p2, p3, p4, p5, p6) {
-      stash[--si] = p4
-          ? p2
-              ? '<img src="' + p4 + '" alt="' + p3 + '"/>'
-              : '<a href="' + p4 + '">' + unesc(highlight(p3)) + '</a>'
-          : p6;
-      return si + '\uf8ff';
+  replace(rx_link, function (all, p1, p2, p3, p4, p5, p6) {
+    stash[--si] = p4
+      ? p2
+        ? '<img src="' + p4 + '" alt="' + p3 + '"/>'
+        : '<a href="' + p4 + '">' + unesc(highlight(p3)) + '</a>'
+      : p6;
+    return si + '\uf8ff';
   });
 
   // table
-  replace(rx_table, function(all, table) {
-      var sep = table.match(rx_thead)[1];
-      return '\n' + element('table',
-          table.replace(rx_row, function(row, ri) {
-              return row == sep ? '' : element('tr', row.replace(rx_cell, function(all, cell, ci) {
-                  return ci ? element(sep && !ri ? 'th' : 'td', unesc(highlight(cell || ''))) : ''
-              }))
-          })
-      )
+  replace(rx_table, function (all, table) {
+    var sep = table.match(rx_thead)[1];
+    return '\n' + element('table',
+      table.replace(rx_row, function (row, ri) {
+        return row == sep ? '' : element('tr', row.replace(rx_cell, function (all, cell, ci) {
+          return ci ? element(sep && !ri ? 'th' : 'td', unesc(highlight(cell || ''))) : ''
+        }))
+      })
+    )
   });
 
   // heading
-  replace(rx_heading, function(all, _, p1, p2) { return _ + element('h' + p1.length, unesc(highlight(p2))) });
+  replace(rx_heading, function (all, _, p1, p2) { return _ + element('h' + p1.length, unesc(highlight(p2))) });
 
   // paragraph
-  replace(rx_para, function(all, content) { return element('p', unesc(highlight(content))) });
+  replace(rx_para, function (all, content) { return element('p', unesc(highlight(content))) });
 
   // stash
-  replace(rx_stash, function(all) { return stash[parseInt(all)] });
+  replace(rx_stash, function (all) { return stash[parseInt(all)] });
 
   return src.trim();
 };
